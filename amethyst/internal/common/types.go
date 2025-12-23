f075e3582b2c5c18c9638ae300ba0befdcc621d6
SuchitraShankar07 <scientist.suchitra@gmail.com>
2025-12-24 03:56:08 +0530

    repo setup

---
+++ amethyst/internal/common/types.go
@@ -0,0 +1,29 @@
+package common
+
+type CompactionType int
+
+const (
+    TIERED CompactionType = iota
+    LEVELED
+)
+
+type SegmentMeta struct {
+    ID            string
+    Offset        int64
+    Length        int64
+
+    MinKey        string
+    MaxKey        string
+
+    Strategy      CompactionType
+
+    ReadCount     int64
+    WriteCount    int64
+    OverlapCount  int64
+
+    CreatedAt     int64
+    LastRewriteAt int64
+
+    Obsolete      bool
+    SparseIndex   interface{} // filled later
+}
